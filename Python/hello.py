327ff83accfa496134abfb98f24384ea5bee3398
Kienan-Baxter <180831@cloud.rangitoto.school.nz>
2025-08-13 12:43:15 +1200

    beans.py
    H

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,77 @@
-print("Hello world!")
+""" I am not fixing the 98 style errors."""
+age = int(input("how old are you\n"))
+year = 2025 - age
+if year > 2007:
+  print ("no")
+elif year < 1:
+  print (h)
+else:
+  h_name = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"#vrable
+  ien = 2
+  til = -6
+  name = input("What is your first name\n")
+  oo = 2+5-6/5*3**2
+  ty = (2+3)*(5-1)
+  q = 6
+  q += 5
+  w = 12
+  w -= 55
+  e = 566
+  e *= 66
+  r = 57877
+  r /= 44
+  t = 7
+  c = 1
+  v = 3
+  print (c==3)
+  condition = t > 5 and t < 10 
+  print (condition)
+  print (q)
+  print (w)
+  print (e)
+  print (r)
+  nmbr = int(input("enter a number\n"))
+  print ("Hello", name)
+  print ("you were born in the", year,"which is old enough")
+  print ("multiplied by 10 =", 10 * nmbr)
+  print (2+2)#int+int
+  print (":3 "*800)#oh god 800 :3
+  print (str(2) + str(2))#22
+  print (7>=2)#more tha of eql >
+  print (8<=56)#less tha or eql <
+  print (2==2)#eql tha =
+  print (1345!=877)#not eql tha !
+  print (72)#72
+  print (888888**13)
+  print (h_name)
+  print (ien + til)
+  print(len(h_name))#made this acidentally   = 76
+  print(oo)
+  print(ty)
+  print((5+3) < (2*5))
+"""
+dw
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+k
+"""
+# +plus -minus *times /divide **power //floor div %remenant
+# string = text, int = whole numbers, float = decimal numbers, boolean = True or False
